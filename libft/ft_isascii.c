1ab836a724236b50dbea7b618d0042d8a1560d0b
SudoQuack <guillaume.astesana@gmail.com>
2025-11-04 16:43:47 +0100

    Added libft as a submodule

--- libft/ft_isascii.c
+++
@@ -1,9 +0,0 @@
-/*
-Tests for an ASCII character, which is any character between 0 and octal 0177 inclusive.
-*/
-int	ft_isascii(int c)
-{
-	if (c >= 0 && c <= 127)
-		return (1);
-	return (0);
-}
--- libft/ft_putchar_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-/*
-Outputs the character ’c’ to the given file descriptor.
-*/
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
